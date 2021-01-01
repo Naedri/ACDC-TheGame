a2b7085fa7b6b6f22fc6b977b845313afe2e4fce
Adrien JALLAIS <adrien.jallais@protonmail.com>
2021-01-01 23:39:00 +0100

    feat(game2): card from hand selection deselection

--- Code/FIL A1 ACDC Partie2 Jallais Adrien/src/view/component/HandComponent.java
+++ Code/FIL A1 ACDC Partie2 Jallais Adrien/src/view/component/HandComponent.java
@@ -6,7 +6,9 @@ package view.component;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -22,31 +24,52 @@ import view.constant.RadiusApp;
 public class HandComponent extends HBox {
 	private List<CardComponent> cardL;
 	private List<StackPane> stackL;
+	private Boolean isOneCardActivated = false;
 
 	public HandComponent(List<CardComponent> listCard, double spacing) {
 		super(spacing);
 		initHand(listCard);
 		setStyle();
+		setAction();
 	}
 
 	public HandComponent(List<CardComponent> listCard) {
 		super();
 		initHand(listCard);
 		setStyle();
+		setAction();
 	}
 
 	private void setStyle() {
-		this.setBackground(new Background(
-				new BackgroundFill(ColorApp.INFOL.getColor(), new CornerRadii(RadiusApp.HIGH.getRadius()), Insets.EMPTY)));
+		this.setBackground(new Background(new BackgroundFill(ColorApp.INFOL.getColor(),
+				new CornerRadii(RadiusApp.HIGH.getRadius()), Insets.EMPTY)));
+	}
+
+	private void setAction() {
+		cardL.forEach(card -> {
+			card.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					if (card.isActive()) {
+						card.switchActive();
+					} else {
+						cardL.forEach(card -> {
+							card.setActive(false);
+						});
+						card.setActive(true);
+					}
+				}
+			});
+		});
 	}
 
 	private void initHand(List<CardComponent> listCard) {
 		this.cardL = listCard;
 		this.stackL = new ArrayList<StackPane>();
+		isOneCardActivated = false;
 
 		cardL.forEach(card -> {
-			StackPane sp = new StackPane();
-			sp.getChildren().addAll(card.makeSupport(), card);
+			StackPane sp = card.makeSupported();
 			this.stackL.add(sp);
 		});
 
