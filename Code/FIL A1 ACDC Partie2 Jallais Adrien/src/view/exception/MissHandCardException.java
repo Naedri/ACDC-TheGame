344d9b23f901a7c330be02103d0127aa4b4e2257
Adrien JALLAIS <adrien.jallais@protonmail.com>
2021-01-02 13:03:19 +0100

    feat(game2): adding exception for layingAction

---
+++ Code/FIL A1 ACDC Partie2 Jallais Adrien/src/view/exception/MissHandCardException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package view.exception;
+
+/**
+ * @author Adrien Jallais
+ *
+ */
+public class MissHandCardException extends Exception {
+
+	/**
+	 * serial number
+	 */
+	private static final long serialVersionUID = 3747754235892759247L;
+
+	public MissHandCardException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ Code/FIL A1 ACDC Partie2 Jallais Adrien/src/view/exception/MissLayCardException.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package view.exception;
+
+/**
+ * @author Adrien Jallais
+ *
+ */
+public class MissLayCardException extends Exception {
+
+	/**
+	 * serial number
+	 */
+	private static final long serialVersionUID = -4855286190791451127L;
+
+	public MissLayCardException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
