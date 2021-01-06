06963968e07736f75c58143b2b3a9194bb5f2fb1
Adrien JALLAIS <adrien.jallais@protonmail.com>
2021-01-06 16:39:26 +0100

    feat(game2): AuthorsScene second try

--- Code/FIL A1 ACDC Partie2 Jallais Adrien/src/view/scene/AuthorsScene.java
+++ Code/FIL A1 ACDC Partie2 Jallais Adrien/src/view/scene/AuthorsScene.java
@@ -1,15 +1,11 @@
 package view.scene;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import application.Main;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 import view.component.DialogComponent;
 import view.constant.FontApp;
-import view.constant.Spacing;
 
 public class AuthorsScene extends MainScene {
 
@@ -17,48 +13,39 @@ public class AuthorsScene extends MainScene {
 	private DialogComponent mainLabel;
 
 	private DialogComponent labelAuthors;
-	private DialogComponent labelAuthorBack;
-	private DialogComponent labelAuthorFront;
-	private DialogComponent labelImageWelcome;
-	private DialogComponent labelImagePlay;
+//	private DialogComponent labelAuthorBack;
+//	private DialogComponent labelAuthorFront;
+//	private DialogComponent labelImageWelcome;
+//	private DialogComponent labelImagePlay;
 
 	public AuthorsScene() {
 		super(new VBox(1));
 		vbox = (VBox) (this.getPane());
-		initLabel();
-		vbox.getChildren().addAll(labelAuthors, labelAuthorBack, labelAuthorFront, labelImageWelcome, labelImagePlay);
-		vbox.setSpacing(Spacing.HIGH.getSpace());
+		initMainLabel();
+		labelAuthors.getDialogL().setFont(FontApp.HIGH.getFont());
+		labelAuthors.getDialogL().setTextAlignment(TextAlignment.LEFT);
+
+		// TODO change raw value
+		vbox.setMaxSize(300, 350);
+		vbox.getChildren().addAll(labelAuthors, mainLabel);
 		vbox.setAlignment(Pos.CENTER_LEFT);
-		vbox.setMaxSize(350, 600);
-		vbox.setBackground(labelAuthors.getBackground());
+		vbox.setBackground(mainLabel.getBackground());
 	}
 
-	private void initLabel() {
+	private void initMainLabel() {
 		labelAuthors = new DialogComponent(Main.d.get("CREDITS_Authors"));
 		labelAuthors.getDialogL().setFont(FontApp.HIGH.getFont());
-		labelAuthorBack = new DialogComponent(Main.d.get("CREDITS_Author_back"));
-		labelAuthorFront = new DialogComponent(Main.d.get("CREDITS_Author_front"));
-		labelImageWelcome = new DialogComponent(Main.d.get("CREDITS_Image_welcome"));
-		labelImagePlay = new DialogComponent(Main.d.get("CREDITS_Image_play"));
 
-		getDialog().forEach(dialog -> {
-			dialog.getDialogL().setTextAlignment(TextAlignment.LEFT);
-			// dialog.getDialogL().setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
-			dialog.getDialogL().setMaxWidth(Double.MAX_VALUE);
-		});
-		labelAuthorBack.addDialog("Adrien Jallais");
-		labelAuthorFront.addDialog("Nicolas Kirchhoffer");
-		labelImageWelcome.addDialog("Momentmal - pixabay.com");
-		labelImagePlay.addDialog("Castle Transparent - pngall.com");
-	}
+		mainLabel = new DialogComponent(Main.d.get("CREDITS_Author_front"));
+		mainLabel.addDialog("- Adrien Jallais");
+		mainLabel.addDialog(Main.d.get("CREDITS_Author_back"));
+		mainLabel.addDialog("- Nicolas Kirchhoffer");
+		mainLabel.addDialog(Main.d.get("CREDITS_Image_welcome"));
+		mainLabel.addDialog("- Momentmal - pixabay.com");
+		mainLabel.addDialog(Main.d.get("CREDITS_Image_play"));
+		mainLabel.addDialog("- Castle Transparent - pngall.com");
 
-	private List<DialogComponent> getDialog() {
-		List<DialogComponent> listDialog = new ArrayList<DialogComponent>();
-		listDialog.add(labelAuthors);
-		listDialog.add(labelAuthorBack);
-		listDialog.add(labelAuthorFront);
-		listDialog.add(labelImageWelcome);
-		listDialog.add(labelImagePlay);
-		return listDialog;
+		mainLabel.getDialogL().setTextAlignment(TextAlignment.LEFT);
+		mainLabel.getDialogL().setMaxWidth(Double.MAX_VALUE);
 	}
 }
